b9e967c91074458002cd463ec165af5127e0d39e
Munawer Taj <taj4580@gmail.com>
2023-07-10 23:36:40 +0600

    Number-2(b) updated

--- NUMBER/2(b) Prime_Check .asm
+++ NUMBER/2(b) Prime_Check .asm
@@ -2,36 +2,55 @@
 .STACK 100H
 
 .DATA
+
 NEWLINE DB 0AH , 0DH , '$'
+MSG1 DB "Enter a digit: $"
 MSGP DB "Number is Prime$"
 MSGN DB "Number is Not Prime$"
+
 .CODE
 
 MAIN PROC
     MOV AX , @DATA
     MOV DS , AX
     
+    ;Printing Input message
+    MOV AH , 9
+    LEA DX , MSG1
+    INT 21H
+    
+    ;Taking input
     MOV AH , 1
     INT 21H
     
-    SUB AL , 48
+    ;Storing the value of AL into BL because of the interruption of NEWLINE later
+    SUB AL , '0'
     MOV BL , AL
     
+    ;Printing newline
     MOV AH , 9
     LEA DX , NEWLINE
     INT 21H
     
-    CMP BL , 2
-    JE PRIME
+    ;If num is less than 2 then it is Not Prime
+    CMP BL, 2
+    JL NOT_PRIME
     
-    CMP BL , 3
-    JE PRIME
+    MOV AH, 0  ;For division operation AH is initialized by 0
+    MOV AL, BL ;Restoring value of num in AL
+    MOV CX, AX
     
-    CMP BL , 5
-    JE PRIME
+    DEC BL
     
-    CMP BL , 7
-    JE PRIME            
+    CHECK_PRIME:
+        CMP BL, 1
+        JE PRIME
+        DIV BL  ; Perform AX/BL and Remainder --> AH Quotient(result) --> AL
+        CMP AH, 0  
+        JE NOT_PRIME
+        DEC BL
+        MOV AX, CX
+        JMP CHECK_PRIME            
     
     NOT_PRIME:
         MOV AH , 9
