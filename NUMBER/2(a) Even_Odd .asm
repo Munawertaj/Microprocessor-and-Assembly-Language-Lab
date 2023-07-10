b7308a79499a235f71f07a63ec6bdb58364c161a
Munawer Taj <taj4580@gmail.com>
2023-07-10 23:22:25 +0600

    Number-2(a) updated

--- NUMBER/2(a) Even_Odd .asm
+++ NUMBER/2(a) Even_Odd .asm
@@ -2,35 +2,48 @@
 .STACK 100H
 
 .DATA
+
 NEWLINE DB 0AH , 0DH , '$'
-EV DB "Number is even$"
-OD DB "Number is odd$"
+MSG1 DB "Enter a digit: $"
+EV DB "Number is Even$"
+OD DB "Number is Odd$"
+
 .CODE
 
 MAIN PROC
     MOV AX , @DATA
     MOV DS , AX
     
+    ;Printing Input message
+    MOV AH , 9
+    LEA DX , MSG1
+    INT 21H
+    
+    ;Taking input
     MOV AH , 1
     INT 21H
     
-    SUB AL , 48
+    ;Storing the value of AL into BL because of the interruption of NEWLINE later
+    SUB AL , '0'
     MOV BL , AL
     
+    ;Printing newline
     MOV AH , 9
     LEA DX , NEWLINE
     INT 21H
     
+    ;Performing AND operation (num & 1) = 0 then num is EVEN else ODD
     MOV AL , 1
     AND BL , AL
     
     CMP BL , 0
     JE EVEN
     
-    MOV AH , 9
-    LEA DX , OD
-    INT 21H
-    JMP EXIT
+    ODD:
+        MOV AH , 9
+        LEA DX , OD
+        INT 21H
+        JMP EXIT
     
     EVEN:
         MOV AH , 9
